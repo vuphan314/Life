093fe0568cea89ea05b1cfabc28fd681c83b122b
Vu Phan <vuphan314@gmail.com>
2017-01-04 19:57:25 +0700

    get all poss grids

--- life.py
+++ life.py
@@ -1,9 +1,17 @@
 #!/usr/bin/python3
 
+from itertools import product
 from typing import List, Set, Tuple
 
+GRID_EDGE = 2
+SEQ_TYPES = {list, tuple} # itertools
+
 Cell = Tuple[int] # pair
-GameState = List[List[bool]]
+CellColl = Set[Cell]
+CellState = bool
+Row = List[CellState]
+Grid = List[Row]
+GridColl = Set[Grid]
 
 class Life:
     lower_survival = 2
@@ -11,71 +19,91 @@ class Life:
     lower_birth = 3
     upper_birth = 3
 
-    def __init__(self, edge=5):
-        self.edge = edge
+    def __init__(self, grid_edge=GRID_EDGE):
+        self.grid_edge = grid_edge
 
-    def get_next_game_state(self, X: GameState) -> GameState:
-        Y = []
-        for i in range(self.edge):
+    def get_image_size() -> int:
+        pass
+
+    def is_in_image(next_grid: Grid) -> bool:
+        pass
+
+    def get_fiber(next_grid: Grid) -> Set[Grid]:
+        pass
+
+    def get_all_poss_grids(self) -> GridColl:
+        cell_states_sets = [{True, False}] * self.grid_edge
+        all_poss_rows = set(product(*cell_states_sets))
+        rows_sets = [all_poss_rows] * self.grid_edge
+        return set(product(*rows_sets))
+
+    def get_next_grid(self, cur_grid: Grid) -> Grid:
+        next_grid = []
+        for i in range(self.grid_edge):
             row = []
-            for j in range(self.edge):
-                C = i, j
-                row.append(self.get_next_cell_state(X, C))
-            Y.append(row)
-        return Y
+            for j in range(self.grid_edge):
+                cell = i, j
+                row.append(self.get_next_cell_state(
+                    cur_grid, cell
+                ))
+            next_grid.append(row)
+        return next_grid
 
-    def get_next_cell_state(self, X: GameState, C: Cell) -> bool:
+    def get_next_cell_state(
+        self, cur_grid: Grid, cell: Cell
+    ) -> CellState:
         num_alive_neighbors = 0
-        for neighbor in self.get_neighbors(C):
-            if self.get_cur_cell_state(X, neighbor):
+        for neighbor in self.get_neighbors(cell):
+            if self.get_cur_cell_state(cur_grid, neighbor):
                 num_alive_neighbors +=1
-        cur_cell_state = self.get_cur_cell_state(X, C)
+        cur_cell_state = self.get_cur_cell_state(
+            cur_grid, cell
+        )
         if cur_cell_state:
-            if is_within(
+            return is_within(
                 num_alive_neighbors,
                 self.lower_survival, self.upper_survival
-            ):
-                return True
-            else:
-                return False
+            )
         else:
-            if is_within(
+            return is_within(
                 num_alive_neighbors,
                 self.lower_birth, self.upper_birth
-            ):
-                return True
-            else:
-                return False
+            )
 
-    def get_cur_cell_state(self, X: GameState, C: Cell) -> bool:
-        return X[C[0]][C[1]]
+    def get_cur_cell_state(
+        self, cur_grid: Grid, cell: Cell
+    ) -> CellState:
+        return cur_grid[cell[0]][cell[1]]
 
-    def get_neighbors(self, C: Cell) -> Set[Cell]:
+    def get_neighbors(self, cell: Cell) -> CellColl:
         D = {-1, 0, 1}
-        rows = {self.wrap(C[0] + d) for d in D}
-        cols = {self.wrap(C[1] + d) for d in D}
-        cells = {(row, col) for row in rows for col in cols}
-        cells.remove(C)
+        row_inds = {self.wrap(cell[0] + d) for d in D}
+        col_inds = {self.wrap(cell[1] + d) for d in D}
+        cells = {(row, col) for row in row_inds for col in col_inds}
+        cells.remove(cell)
         return cells
 
     def wrap(self, val: int) -> int:
-        return val % self.edge
+        return val % self.grid_edge
 
 def is_within(m: int, l: int, r: int):
     return l <= m and m <= r
 
+def printp(o: object) -> None:
+    if type(o) in SEQ_TYPES and o and type(o[0]) in SEQ_TYPES:
+    # isinstance(o, Grid)
+        print('Grid: [')
+        for row in o:
+            print(row)
+        print(']')
+    elif isinstance(o, GridColl):
+        print('GridColl: {')
+        for grid in o:
+            printp(grid)
+        print('}')
+    else:
+        print(o)
+
 l = Life()
-n = 5
-X = [[1 for i in range(n)] for j in range(n)]
-p = l.get_next_game_state(X)
-print(X, p)
-
-
-def get_image_size(n: int) -> int:
-    pass
-
-def is_in_image(n: int, Y: GameState) -> bool:
-    pass
-
-def get_fiber(n: int, Y: GameState) -> Set[GameState]:
-    pass
+p = l.get_all_poss_grids()
+printp(p)
