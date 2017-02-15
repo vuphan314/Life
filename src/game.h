0cfeca8a44fc70941703857f789f4e9d1ce8abbc
Vu Phan <vuphan314@gmail.com>
2017-02-14 22:59:04 -0600

    header

--- src/game.h
+++ src/game.h
@@ -3,15 +3,38 @@
 
 ////////////////////////////////////////////////////////////
 
-using Byte = unsigned char;
-using CellStateByte = Byte;
-using Grid = vector<CellStateByte>;
-using GridStateByte = Byte;
-using GridStateCollection = vector<GridStateByte>;
+using Char = unsigned char; // n
+using Int = unsigned int; // n^2
+using Long = unsigned long long; // 2^n^2
+
+using CellState = bool;
+using Row = vector<CellState>;
+using Grid = vector<Row>;
+
+using GridState = bool;
+using Image = vector<GridState>;
 
 ////////////////////////////////////////////////////////////
 
-const Byte BYTE = 8;
+const Char LOWER_SURVIVAL = 2;
+const Char UPPER_SURVIVAL = 3;
+const Char LOWER_BIRTH = 3;
+const Char UPPER_BIRTH = 3;
+
+////////////////////////////////////////////////////////////
+
+void setImage(Image &image, const Char &order);
+
+void setNextGrid(Grid &nextGrid, const Grid &grid);
+
+CellState getNextCellState(const Grid &grid,
+  const Char rowIndex, const Char columnIndex);
+
+Char getAliveNeighborCount(const Grid &grid,
+  const Char rowIndex, const Char columnIndex);
+
+CellState isAlive(const Grid &grid,
+  const Char rowIndex, const Char columnIndex);
 
 ////////////////////////////////////////////////////////////
 
